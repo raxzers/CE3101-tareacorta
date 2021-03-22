e4f5758939e1784cd2c2f86798e2d84fa3e7befb
Manuel Jiménez <manolos5535@hotmail.com>
2021-03-21 19:12:52 -0600

    Cambio1Atras
    Vuelta  a subir

---
+++ tarea corta/ServidorRest/tareaCorta1-Rest/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi_csharp.Modelos
+{
+    public class Cliente
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string address3 { get; set; }
+        public string Phone { get; set; }
+        public string Bday { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ tarea corta/ServidorRest/tareaCorta1-Rest/Repositories/RPClientes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using webapi_csharp.Modelos;
+
+namespace webapi_csharp.Repositorios
+{
+    public class RPClientes
+    {
+        public static List<Cliente> _listaClientes = new List<Cliente>()
+        {
+            new Cliente() { Id = "1", Name = "Cliente 1" , LastName = "Apellido 1", address1 = "Cartago", address2 = "Cartago", address3 = "Cartago" , Bday = "28/02/1995" , Mail = "CartagitoCampeon@cartago.es" , Password = "xq405010" , Phone = "85412364" },
+            new Cliente() { Id = "2", Name = "Cliente 2" , LastName = "Apellido 2", address1 = "Cartago", address2 = "Cartago", address3 = "San Nicolas" , Bday = "28/05/1995" , Mail = "CartagitoCampeon@hostmail.es" , Password = "123456789" , Phone = "88552233"  },
+            new Cliente() { Id = "3", Name = "Cliente 3" , LastName = "Apellido 3", address1 = "Cartago", address2 = "Cartago", address3 = "Guadalupe" , Bday = "28/09/1995" , Mail = "CartagitoCampeon@gamil.com" , Password = "Cartago123" , Phone = "85445645"  }
+        };
+
+        public IEnumerable<Cliente> ObtenerClientes()
+        {
+            return _listaClientes;
+        }
+
+        public Cliente ObtenerCliente(string id)
+        {
+            var cliente = _listaClientes.Where(cli => cli.Id == id);
+
+            return cliente.FirstOrDefault();
+        }
+
+        public void Agregar(Cliente nuevoCliente)
+        {
+            _listaClientes.Add(nuevoCliente);
+        }
+    }
+}
